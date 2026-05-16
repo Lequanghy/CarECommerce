a5930f6d05b06650849382628c9474f46e29ac02
Quang Hy Le <Lequanghy1998@yahoo.ca>
2026-05-16 00:10:58 -0400

    feat: update UI in search page

--- resources/views/car/search.blade.php
+++ resources/views/car/search.blade.php
@@ -10,7 +10,7 @@
                                  stroke="currentColor" style="width: 20px">
                                  <path stroke-linecap="round" stroke-linejoin="round"
                                      d="M6 13.5V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 9.75V10.5" />
-                                 </svg>
+                             </svg>
                              Filters
                          </button>
                          <div>
@@ -42,7 +42,8 @@
 
                          <!-- Find a car form -->
                          <section class="find-a-car">
-                             <form action="{{ route('car.search') }}" method="GET" class="find-a-car-form card flex p-medium">
+                             <form action="{{ route('car.search') }}" method="GET"
+                                 class="find-a-car-form card flex p-medium">
                                  <div class="find-a-car-inputs">
                                      <div class="form-group">
                                          <label class="mb-medium">Maker</label>
